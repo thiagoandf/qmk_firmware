147b051c6c7ffa4492985a375be2664cc88b47ff
Thiago de Andrade Feliciano <thiagoandf@gmail.com>
2023-09-05 10:58:22 +0200

    add wpm and acc info

--- keyboards/sofle/keymaps/taf/oled.c
+++ keyboards/sofle/keymaps/taf/oled.c
@@ -1,32 +1,67 @@
- /* Copyright 2020 Josef Adamcik
-  * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-//Sets up what the OLED screens display.
+// Sets up what the OLED screens display.
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 #ifdef OLED_ENABLE
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
+// Variables to keep track of typing speed and accuracy
+static uint32_t start_time  = 0;
+static uint32_t key_count   = 0;
+static uint32_t error_count = 0;
 
-    oled_write_P(qmk_logo, false);
+void print_wpm(void) {
+    // Increment key count for each keypress
+    if (timer_elapsed(start_time) > 1000) { // Update every second
+        oled_clear();
+        oled_write_P(PSTR("Speed: "), false);
+
+        uint32_t elapsed_time = timer_elapsed(start_time) / 1000; // Convert to seconds
+        uint32_t typing_speed = key_count / elapsed_time;
+
+        char speed_str[8];
+        snprintf(speed_str, sizeof(speed_str), "%lu WPM", (unsigned long)typing_speed);
+        oled_write(speed_str, false);
+
+        float accuracy = 100.0 - (error_count * 100.0 / key_count);
+        char  accuracy_str[8];
+        snprintf(accuracy_str, sizeof(accuracy_str), "Acc: %.2f%%", accuracy);
+        oled_write_P(PSTR("\n"), false);
+        oled_write(accuracy_str, false);
+
+        // Reset counts and start time for the next calculation
+        key_count   = 0;
+        error_count = 0;
+        start_time  = timer_read();
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        // Increment key count on keypress
+        key_count++;
+    } else if (record->event.key.col != QK_LOCK) { // Use QK_LOCK instead of QK_NO_KEY
+        // Increment error count on key release if a valid key was pressed
+        error_count++;
+    }
+    return true;
 }
 
 static void print_status_narrow(void) {
@@ -77,7 +112,7 @@ bool oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
-        render_logo();
+        print_wpm();
     }
     return false;
 }
